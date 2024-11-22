139efae5488e8ac1ab4a7b898603e64c7fd1b369
bartek5916 <bartek5916@o2.pl>
2024-11-22 21:50:48 +0100

    Sidebar look update

--- orion-casino-gui/src/components/Sidebar/Sidebar.jsx
+++ orion-casino-gui/src/components/Sidebar/Sidebar.jsx
@@ -8,18 +8,40 @@ function Sidebar() {
         <>
             <nav className="sidebar">
                 <div className="sidebar-container">
+
+                    <div className="free-spin-button">
+                        <img className="free-spin-icon" src="/assets/images/sidebar/free-spin.png"
+                             alt="freespin-icon"></img>
+                        <div className="free-spin-text">FREE SPIN</div>
+                        <img className="free-spin-icon" src="/assets/images/sidebar/free-spin.png"
+                             alt="freespin-icon"></img>
+                    </div>
+
                     <div className="main-tiles">
                         <ol>
-                            <SidebarButton text="O nas" icon="/assets/images/sidebar/about-us.png"/>
+                        <div className="popular-games">
+                                <SidebarButton text="Popularne gry" icon="/assets/images/sidebar/popular-icon.png"/>
+                            </div>
+                            <div className="new-games">
+                                <SidebarButton text="Nowe gry" icon="/assets/images/sidebar/new-games.png"/>
+                            </div>
+                            <div className="facourite-games">
+                                <SidebarButton text="Ulubione gry" icon="/assets/images/sidebar/favourite-icon.png"/>
+                            </div>
+                            <div className="deposit">
+                                <SidebarButton text="Depozyt" icon="/assets/images/sidebar/deposit-icon.png"/>
+                            </div>
+                            <div className="leaderboard">
+                                <SidebarButton text="Ranking" icon="/assets/images/sidebar/ranking-icon.png"/>
+                            </div>
+                            <div className="about-us">
+                                <SidebarButton text="O nas" icon="/assets/images/sidebar/about-us.png"/>
+                            </div>
                         </ol>
                     </div>
 
                     <div className="support-button">
-                        <SidebarButton text="Support"/>
-                    </div>
-
-                    <div className="deposit-button">
-                        <ButtonGradient text="Depozyt"/>
+                        <SidebarButton text="Wsparcie" icon="/assets/images/sidebar/faq-icon.png"/>
                     </div>
 
                 </div>
